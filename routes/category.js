cb2edc7f46a42e0f3d478cb9e65d42aacb1954c6
Dministry <dministry356@gmail.com>
2021-11-09 16:55:27 +0100

    progress

--- routes/category.js
+++ routes/category.js
@@ -29,7 +29,7 @@ router.get('/categories', async (req, res) => {
         res.json({
             success: true,
             categories: categories
-        })
+        });
     } catch (err) {
         res.status(500).json({
             success: false,
